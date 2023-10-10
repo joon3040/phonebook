7d742c04f7730e63a6d43e4d6696d1f32e853481
bitna kim <tallshe108@hanmail.net>
2023-10-10 11:44:11 +0700

    Update reducer.js

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -6,17 +6,19 @@ const contactReducer = (state = initialState, action) => {
   let { type, payload } = action;
   switch (type) {
     case "ADD_CONTACT":
-      state.contact.push({
-        name: payload.name,
-        phoneNumber: payload.phoneNumber,
-      });
-      break;
-    case "SEARCH_BY_USERNAME":
-      state.keyword = payload.keyword;
-      break;
-  }
+      return {
+        ...state,
+        contact: [
+          ...state.contact,
+          { name: payload.name, phoneNumber: payload.phoneNumber },
+        ],
+      };
 
-  return { ...state };
+    case "SEARCH_BY_USERNAME":
+      return { ...state, keyword: payload.keyword };
+    default:
+      return { ...state };
+  }
 };
 
 export default contactReducer;
